b21dd11c9637bbcfa897f7e74901589cf12a5b23
yellow-sky <nikulin.e@gmail.com>
2015-10-21 03:57:02 +0300

    Update BaseMapInfo

--- base_map_info.py
+++ base_map_info.py
@@ -3,11 +3,14 @@ __license__ = 'GPLv2'
 __date__ = '2014'
 
 
-class BaseMapInfo():
+class BaseMapInfo:
 
-    def __init__(self, id=None, label=None, thumb_path=None, uri=None, linked_obj=None):
+    def __init__(self, id=None, label=None, thumb_path=None, uri=None, linked_obj=None, is_grayscale=False, brightness=0):
         self.id = id
         self.label = label
         self.thumb_path = thumb_path
         self.uri = uri
         self.linked_obj = linked_obj
+
+        self.is_grayscale = is_grayscale
+        self.brightness = brightness
